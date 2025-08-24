76d0a56b54ae7616bcbab1f6a3f52b1ddc296e77
Mohammadsyedmostafa <syedmostafa884@gmail.com>
2025-08-25 02:01:40 +0600

    added Transition Section

--- javascript/home.js
+++ javascript/home.js
@@ -1,17 +1,59 @@
-const pin = "1234"
+const pin = 1234
+
+// funtion for input value
+function getInputNumber(id){
+    const InputField = document.getElementById(id)
+    const inputFieldValue = InputField.value
+    const inputFieldValueNumber = parseInt(inputFieldValue)
+    return inputFieldValueNumber
+}
+
+function getInputValue(id){
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value
+    return inputFieldValue
+}
+function inputInnerText(id){
+    const inputInner = document.getElementById(id)
+    const inputInnerValue = inputInner.innerText
+    const inputInnerValueNumber = parseInt(inputInnerValue)
+    return inputInnerValueNumber
+}
+// funtion to set InnerText
+function setInnerTaxt(value){
+    const availableBalanceElement = document.getElementById('availableBalance')
+    availableBalanceElement.innerText = value
+}
+
+// Funtion to Toggol
+function toggole(id){
+    const forms = document.getElementsByClassName('form')
+    for(const form of forms){
+        form.style.display = "none"
+    }
+    document.getElementById(id).style.display = "block"
+}
+// Funtion to toggol Button
+function toggolButton(id){
+    const formbtns = document.getElementsByClassName("form-btn")
+    for(const btn of formbtns){
+        btn.classList.remove("border-[#0874f2]" , "bg-[#0874f20d]")
+        btn.classList.add("border-[#0808081a]")
+    }
+    document.getElementById(id).classList.remove("border-stone-300")
+    document.getElementById(id).classList.add("border-[#0874f2]" , "bg-[#0874f20d]")
+
+}
+
 
 document.getElementById('add-money-btn').addEventListener('click', function(e){
     e.preventDefault()
-    const bank = document.getElementById('bank').value
-
-    const accountNumber = document.getElementById('account-number').value
-
-    const addAmount = parseInt(document.getElementById('add-amount').value)
-
-    const pinNumber = document.getElementById('pin-number').value
+    const bank = getInputValue('bank')
+    const accountNumber = getInputValue('account-number')
+    const addAmount = getInputNumber('add-amount')
+    const pinNumber = getInputNumber('pin-number')
     console.log(bank,accountNumber,addAmount,pinNumber)
-
-    const availableBalance = parseInt(document.getElementById('availableBalance').innerText)
+    const availableBalance = inputInnerText('availableBalance')
     console.log(availableBalance)
 
     if(accountNumber.length <11){
@@ -25,8 +67,7 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
     }
 
     const totalAvailableBalance = addAmount + availableBalance
-
-    document.getElementById('availableBalance').innerText = totalAvailableBalance
+    setInnerTaxt(totalAvailableBalance)
 })
 
 
@@ -34,14 +75,11 @@ document.getElementById('add-money-btn').addEventListener('click', function(e){
 
 document.getElementById('withdraw-money-btn').addEventListener('click', function(e){
     e.preventDefault()
-    const withdrawAccountNumber = document.getElementById('withdraw-account-number').value
-
-    const withdrawAddAmount = parseInt(document.getElementById('withdraw-add-amount').value)
-
-    const withdrawPinNumber = document.getElementById('withdraw-pin-number').value
+    const withdrawAccountNumber = getInputValue('withdraw-account-number')
+    const withdrawAddAmount = getInputNumber('withdraw-add-amount')
+    const withdrawPinNumber = getInputNumber('withdraw-pin-number')
     console.log(withdrawAccountNumber,withdrawAddAmount,withdrawPinNumber)
-
-    const availableBalance = parseInt(document.getElementById('availableBalance').innerText)
+    const availableBalance = inputInnerText('availableBalance')
     console.log(availableBalance)
     
     if(withdrawAccountNumber.length <11){
@@ -54,18 +92,36 @@ document.getElementById('withdraw-money-btn').addEventListener('click', function
     }
 
     const totalAvailableBalance = availableBalance - withdrawAddAmount
-    document.getElementById('availableBalance').innerText = totalAvailableBalance
+    setInnerTaxt(totalAvailableBalance)
 })
 
 
 // toggoling
 
 document.getElementById('add-money-botton').addEventListener('click',function(){
-    document.getElementById('cash-out').style.display ="none"
-    document.getElementById('add-money').style.display ="block"
+    toggole("add-money")
+    toggolButton('add-money-botton')
 })
 
 document.getElementById('cash-out-botton').addEventListener('click',function(){
-    document.getElementById('add-money').style.display ="none"
-    document.getElementById('cash-out').style.display ="block"
+    toggole("cash-out")
+    toggolButton('cash-out-botton')
 })
+
+document.getElementById('transfer-botton').addEventListener('click',function(){
+    toggole("transfer-money")
+    toggolButton('transfer-botton')
+})
+document.getElementById('bonus-botton').addEventListener('click',function(){
+    toggole("bonus-money")
+    toggolButton('bonus-botton')
+})
+document.getElementById('pay-botton').addEventListener('click',function(){
+    toggole("pay-bill")
+    toggolButton('pay-botton')
+})
+document.getElementById('transacition-botton').addEventListener('click',function(){
+    toggole("transacition-money")
+    toggolButton('transacition-botton')
+})
+
